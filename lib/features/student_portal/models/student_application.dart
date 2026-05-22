0f01ce97e4cc420b77a84fa50d8dbbedf17148b7
Tshepi-so-spec <224079447@stud.cut.ac.za>
2026-05-22 09:25:19 +0200

    Tshepiso Mofokeng 224079447 - Student Application Model contribution

--- lib/features/student_portal/models/student_application.dart
+++ lib/features/student_portal/models/student_application.dart
@@ -1,7 +1,8 @@
 /**
- * Student Numbers: [Student Number 1], [Student Number 2], [Student Number 3], [Student Number 4], [Student Number 5]
- * Student Names  : [Full Name 1], [Full Name 2], [Full Name 3], [Full Name 4], [Full Name 5]
- * Question: Student Application Model
+ * Student Numbers: 224079447, 220039413, 222068206, 224004059
+ * Student Names  : Tshepiso Mofokeng, Dlali Ntlahla,
+ *                  Odwa Cengimbo, Tshitso Selepe
+ * Question: Student Application Model - Tshepiso Mofokeng
  */
 class StudentApplication {
   final String  id;
