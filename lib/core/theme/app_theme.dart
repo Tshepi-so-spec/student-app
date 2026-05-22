d9eb86dff11d48a7dc4d6c8d66af8b772e9daeb0
Tshepi-so-spec <224079447@stud.cut.ac.za>
2026-05-22 09:22:42 +0200

    Update app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,7 +1,8 @@
 /**
- * Student Numbers: [Student Number 1], [Student Number 2], [Student Number 3], [Student Number 4], [Student Number 5]
- * Student Names  : [Full Name 1], [Full Name 2], [Full Name 3], [Full Name 4], [Full Name 5]
- * Question: App Theme
+ * Student Numbers: 224079447, 220039413, 222068206, 224004059
+ * Student Names  : Tshepiso Mofokeng, Dlali Ntlahla,
+ *                  Odwa Cengimbo, Tshitso Selepe
+ * Question: App Theme - Tshepiso Mofokeng
  */
 import 'package:flutter/material.dart';
 
